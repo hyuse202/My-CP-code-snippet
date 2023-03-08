001685da7cc55e4cfb30d8233641428d4dd7c245
hyuse202 <99476925+hyuse202@users.noreply.github.com>
2023-03-08 20:02:31 +0700

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,22 +1,14 @@
- #include <bits/stdc++.h>
+// Author hyuse202
+// Big Inspired by orzdevinwang
+#include<bits/stdc++.h>
+#define L(i, j, k) for(int i = (j); i <= (k); ++i)
+#define L0(i, j, k) for(int i = (j); i < (k); ++i)
+#define ll long long
+#define int ll
+#define ull unsigned long long
+#define siz(a) ((int) (a).size())
+#define me(a, x) memset(a, x, sizeof(a))
 using namespace std;
-
-using ll = long long;
-#define endl             '\n'
-#define int              long long
-#define pii              pair<int,int>
-#define fi               first
-#define se               second
-#define all(x)           (x).begin(), (x).end()
-#define sz(x)            ((int)(x).size())
-#define For(i, a, b) for(int i = (a); i <= (b); i++)
-#define Rep(i, a, b) for(int i = (a); i < (b); i++)
-#define y0 y2002
-#define y1 y2006
-
-template<class T1, class T2> bool chmin(T1 &x, T2 y) { return (x > y ? x = y, 1 : 0); }
-template<class T1, class T2> bool chmax(T1 &x, T2 y) { return (x < y ? x = y, 1 : 0); }
-
 
 template <typename T>
 void _print(const T &x, int n){
@@ -29,23 +21,23 @@ void _print(const T &x, int n){
 #define WA assert(false)
 #define db(x) cerr << "[ " << #x << " ]" << " = " << "[ " << x <<" ], "
 
-/// Code goes here
-const int mxN = 2e5 + 7;
+// Code goes here
+const int N = 2e5 + 7;
 
-void solve(){
+void Main(){
 
 }
 
-int32_t main()
-{
+int32_t main(){
+
 #ifndef ONLINE_JUDGE
     freopen("inp.txt", "r", stdin);
-#endif // ONLINE_JUDGE
-
+#endif
     ios::sync_with_stdio(false);
     cin.tie(0);
-    solve();
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+    Main();
     return 0;
 }
-
-
